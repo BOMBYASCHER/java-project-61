40fe51dc7e9190e586f4d60158174fb092831bb2
BOMBYASCHER <bombyascher@gmail.com>
2023-03-21 13:31:22 +0300

    Try to fix

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -13,10 +13,12 @@ public class Cli {
         return username;
     }
     public static String start() {
+        System.out.println("/May I have you name?/");
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("5 - Progression");
         System.out.println("6 - Prime");
         System.out.println("0 - Exit");
